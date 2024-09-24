076b0445053593c087793f4e5848ad35d3e778f0
thehogue <83727857+thehogue@users.noreply.github.com>
2024-09-24 15:26:00 +0100

    Create  Contact.js

---
+++ Contact.js
@@ -0,0 +1,19 @@
+
+alert("Welcome to the Contact Us page");
+
+// TASK 4B – Copy your web app's Firebase configuration here ...
+
+
+// TASK 4C
+
+
+
+// Task 2D - Tell JavaScript to call saveContacts when SUBMIT button is clicked
+
+
+
+// Task 5B - Code to retrieve and display the data goes here ...
+
+
+
+// Task 5C - Code to retrieve and display the data in a list goes here ...
